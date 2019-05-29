2eb519e3d4dce12ca37a86c1019eaa69bdcda261
1263919792@qq.com <1w2b3c4g>
2019-05-29 23:34:37 +0800

    IdleStateHandler读写超时时间监控handler

---
+++ src/main/java/nettytimeout/EchoServerHandler.java
@@ -0,0 +1,33 @@
+package nettytimeout;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleStateEvent;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+
+            String eventType = null;
+
+            switch (event.state()) {
+                case READER_IDLE:
+                    eventType = "读超时";
+                    break;
+
+                case WRITER_IDLE:
+                    eventType = "写超时";
+                    break;
+
+                case ALL_IDLE:
+                    eventType = "读写超时";
+                    break;
+            }
+
+            System.out.println(ctx.channel().remoteAddress() + " 超时时间 : " + eventType);
+        }
+    }
+}
